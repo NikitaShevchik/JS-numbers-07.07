6f1233f7d099dfaf8b9a8967c235d2e5e91a75ad
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-08 14:40:07 +0300

    done onbeforeunload + logic game

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,9 +4,11 @@ const gameBoard = document.querySelector('.guess__board')
 let colsRows = [2, 2]; // колонки и ряды
 let maxNumber = colsRows[0] * colsRows[1]; //нужно для будущего. Максимальное число (колонки * ряды)
 let randomNumbers = []; // пустой массив для записи сгенерированных чисел (количество чисел - maxNumber)
-
+let clicks;
+let win;
 /*---------Генерация игрового поля----------*/
 function boardGenerator(arr, arrRandoms) {
+    arrRandoms = [];
     gameBoard.innerHTML = '';
     function boardRowsColsGenerator(arr) {
         for (let i = 0; i < arr[0]; i++) {
@@ -37,7 +39,6 @@ function boardGenerator(arr, arrRandoms) {
             return ran;
         }
     }
-
     function numberInItems(randomArr) {
         const allItems = gameBoard.querySelectorAll('.board__item');
         for (let i = 0; i < allItems.length; i++) {
@@ -45,21 +46,14 @@ function boardGenerator(arr, arrRandoms) {
         }
     }
     boardRowsColsGenerator(arr)
-    randomArrayGen(arrRandoms)
-    numberInItems(arrRandoms)
+    setTimeout(() => randomArrayGen(arrRandoms), 20);
+    setTimeout(() => numberInItems(arrRandoms), 30)
+    setTimeout(eventsForItems, 40)
 }
-/*---------Увеличитель массива рядов и колонок----------*/
-function arrInk() {
-    colsRows = [colsRows[0] + 1, colsRows[1] + 1];
-    maxNumber = colsRows[0] * colsRows[1];
-}
-arrInk()
-boardGenerator(colsRows, randomNumbers)
-
-
 function eventsForItems() {
     const allItems = gameBoard.querySelectorAll('.board__item');
-    var clicks = 1;
+    clicks = 1;
+    win = false;
     for (let item of allItems) {
         item.addEventListener('click', function (e) {
             if (item.innerHTML == clicks) {
@@ -69,7 +63,6 @@ function eventsForItems() {
                 e.preventDefault()
             }
             function checkWin() {
-                var win;
                 for (let i = 0; i < maxNumber; i++) {
                     if (allItems[i].classList.contains('_selected')) {
                         win = true;
@@ -85,9 +78,36 @@ function eventsForItems() {
                 }
             }
             if (checkWin()) {
-                console.log('ПОБЕДА')
+                arrInk();
+                setTimeout(() => boardGenerator(colsRows, randomNumbers), 50)
             }
         })
     }
 }
-eventsForItems()
+/*---------Увеличитель массива рядов и колонок----------*/
+function arrInk() {
+    colsRows = [colsRows[0] + 1, colsRows[1] + 1];
+    maxNumber = colsRows[0] * colsRows[1];
+}
+
+
+// arrInk()
+// boardGenerator(colsRows, randomNumbers)
+// eventsForItems()
+
+boardGenerator(colsRows, randomNumbers)
+
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 116) {
+        console.log('reload')
+        let rel = confirm('При перезагрузке страницы, вы потеряете ВЕСЬ прогресс. Вы уверены?');
+        if (!rel) {
+            e.preventDefault();
+        }
+    }
+})
+window.onbeforeunload = function () {
+    return false;
+}
+
